82f1333fc0d6d579de0141e5f3fe2c38a2e6334c
Izabela Karennina Travizani Maffra <bela@Izabelas-MacBook-Air.local>
2014-03-25 23:23:10 -0300

    Finished

--- src/analyze.py
+++ src/analyze.py
@@ -1,19 +1,59 @@
 import os
 
+from itertools import combinations
+from itertools import product
+
 import networkx as nx
 
 
 def read_graph():
+    """Generator that reads pickled graphs."""
     data_dir = os.path.join(os.path.dirname(__file__), '../data')
     for _, _, file_names in os.walk(data_dir):
         for file_name in file_names:
             if file_name.endswith('.gpickle'):
-                graph = nx.read_gpickle(file_name)
+                path = data_dir + '/' + file_name
+                graph = nx.read_gpickle(path)
+                print 'Read graph from %s' % path
                 yield graph
+
+def is_balanced(triangle_signs):
+    count = triangle_signs.count(1)
+    if (count % 2):
+        return False
+    return True
+
+
+def find_triangles(graph):
+    """Find triangles in graph."""
+    nodes = graph.nodes()
+    for node in nodes:
+        neigh = graph.neighbors(node)
+        pairs = combinations(neigh, 2)
+        for (a, b) in pairs:
+            if graph.has_edge(a, b):
+                weights1 =  [data.values()[0] for data in graph.get_edge_data(a,
+                    b).values()]
+                weights2 =  [data.values()[0] for data in graph.get_edge_data(node,
+                    a).values()]
+                weights3 =  [data.values()[0] for data in graph.get_edge_data(node,
+                    b).values()]
+                for triangle in product(weights1, weights2, weights3):
+                    yield triangle
+        graph.remove_node(node)
 
 
 def main():
     for graph in read_graph():
+        balanced = 0
+        unbalanced = 0
+        for triangle in find_triangles(graph):
+            if is_balanced(triangle):
+                balanced += 1
+            else:
+                unbalanced += 1
+        print 'Balanced triangles: %d' % balanced
+        print 'Unbalanced triangles: %d' % unbalanced
 
         
 if __name__ == '__main__':
--- src/read_input.py
+++ src/read_input.py
@@ -9,10 +9,25 @@ def main():
         for file_name in file_names:
             if file_name.endswith('.gz'):
                 print 'Reading data from file %s' % file_name
-                graph = nx.read_weighted_edgelist(file_name, create_using=nx.DiGraph())
-                out_name = file_name.replace('.gz', '.gpickle')
+                path = DATA_DIR + file_name
+                graph = nx.read_weighted_edgelist(path, create_using=nx.DiGraph())
+                out_name = path.replace('.gz', '.gpickle')
                 print 'Saving data to %s' % out_name
+                graph = directed_to_undirected_multi(graph)
                 nx.write_gpickle(graph, out_name)
+
+
+def directed_to_undirected_multi(digraph):
+    """Transform a directed graph in an unidrected multigraph.
+    >>> graph = nx.DiGraph()
+    >>> graph.add_path([1, 2, 3, 1], weight=1)
+    >>> graph.add_edge(1, 3, weight=0)
+    >>> directed_to_undirected_multi(graph) 
+    """
+    multigraph = nx.MultiGraph()
+    multigraph.add_edges_from(digraph.edges(data=True))
+    return multigraph
+
 
 if __name__ == '__main__':
      main()
